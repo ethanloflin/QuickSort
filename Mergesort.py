07cb244ef9c0760a07c2719d6e648bf89b5ffcca
ethanloflin <86082581+ethanloflin@users.noreply.github.com>
2022-06-13 15:34:16 -0500

    Update Mergesort.py

--- Mergesort.py
+++ Mergesort.py
@@ -1,40 +1,52 @@
+
 def merge_sort(arr):
-	if len(arr) <= 1:
+		if len(arr) <= 1:
 			return arr
-	mid = len(arr)//2
-	left = arr[:mid]
-	right = arr[mid:]
+		
+		mid = len(arr)//2
+		
+		left = arr[:mid]
+		right = arr[mid:]
 	
-	left = merge_sort(left)
-	right = merge_sort(right)
+		merge_sort(left)
+		merge_sort(right)
 	
-	return merge_two_sorted_lists(left, right)
+		merge_two_sorted_lists(left, right)
 	
 def merge_two_sorted_lists(a,b):
-	sorted_list = [] 
-	len_a = len(a)
-	len_b = len(b)
-	i = j = 0
-		
-	while i < len_a and j < len_b:
-			if a[i] <= b[j]:
-				sorted_list.append(a[i])
-				i+=1
-			else:
-				sorted_list.append(b[j])
-				j+=1
-				
-	while i < len_a:
-			sorted_list.append(a[i])
-			i+=1
-		
-	while i < len_b:
-			sorted_list.append(b[j])
-			j+=1
+		len_a = len(a)
+		len_b = len(b)
 	
-		return sorted_list
+		i = j = k = 0
+		
+		while i < len_a and j < len_b:
+				if a[i] <= b[j]:
+						arr[k] = a[i]
+						i+=1
+				else:
+						arr[k] = b[j]
+						j+=1
+				k +=1
+				
+		while i < len_a:
+				arr[k] = b[j]
+				i+=1
+				k+=1
+		
+		while i < len_b:
+				arr[k] = b[j]
+				j+=1
+				k+=1
 
 if __name__ == '__main__':
-		arr = [33,22, 45, 82, 77,99,44,11,54]
-		
-		print(merge_two_sorted_lists(arr))
+        test_cases = [
+        [10, 3, 15, 7, 8, 23, 98, 29],
+        [],
+        [3],
+        [9,8,7,2],
+        [1,2,3,4,5]
+    ]
+    
+    for arr in test_cases:
+        merge_sort(arr)
+        print(arr)
