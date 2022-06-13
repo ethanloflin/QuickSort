3f9225ddd5db58fe7d41f1e2276c5e11b7dfc3db
ethanloflin <86082581+ethanloflin@users.noreply.github.com>
2022-06-13 15:15:33 -0500

    Update Mergesort.py

--- Mergesort.py
+++ Mergesort.py
@@ -1,22 +1,40 @@
-
+def merge_sort(arr):
+	if len(arr) <= 1:
+			return arr
+	mid = len(arr)//2
+	left = arr[:mid]
+	right = arr[mid:]
+	
+	left = merge_sort(left)
+	right = merge_sort(right)
+	
+	return merge_two_sorted_lists(left, right)
+	
 def merge_two_sorted_lists(a,b):
-		sorted_list = [] 
-		len_a = len(a)
-		len_b = len(b)
-		i = j = 0
+	sorted_list = [] 
+	len_a = len(a)
+	len_b = len(b)
+	i = j = 0
 		
-		while i < len_a and j < len_b:
-				if a[i] <= b[j]:
-						sorted_list.append(a[i])
-						i+=1
-				else:
-						sorted_list.append(b[j])
-						j+=1
+	while i < len_a and j < len_b:
+			if a[i] <= b[j]:
+				sorted_list.append(a[i])
+				i+=1
+			else:
+				sorted_list.append(b[j])
+				j+=1
+				
+	while i < len_a:
+			sorted_list.append(a[i])
+			i+=1
 		
+	while i < len_b:
+			sorted_list.append(b[j])
+			j+=1
+	
 		return sorted_list
 
 if __name__ == '__main__':
-		a = [33,22,45,82,77]
-		b = [99,44,11,54]
+		arr = [33,22, 45, 82, 77,99,44,11,54]
 		
-		print(merge_two_sorted_lists(a,b))
+		print(merge_two_sorted_lists(arr))
